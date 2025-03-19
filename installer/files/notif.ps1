e760782110f64f8f1cc4019b8c9eb1ce3c26a50d
Andrebeck27 <84735477+Andrebeck27@users.noreply.github.com>
2025-03-19 21:45:28 +0100

    add mp4link & move stuff around to improve speed

--- installer/files/notif.ps1
+++ installer/files/notif.ps1
@@ -1,15 +1,24 @@
 . ./settings.ps1
-$message = Get-Content response.txt | Select-Object -Index 1
-$url = Get-Content response.txt | Select-Object -Index 2
-$messageOut = ($message -replace '.*    "message": "' -replace '",.*')
-$urlOut = ($url -replace '.*    "file_url": "' -replace '",.*')
-if ($LinkOut -ne 0){
+if ($LinkOut){
+    $url = Get-Content response.txt | Select-Object -Index 2
+    $urlOut = ($url -replace '.*    "file_url": "' -replace '",.*')
+    if ($urlOut -like '*.mp4'){
+        $linkOut = $Mp4Link
+    }
     if ($linkOut -eq 2){
         $replacereg = '\..*$'
         $urlOut = ($urlOut -replace '.*/' -replace $replacereg)
         $urlOut = ("https://files.vc/d/dl?hash="+$urlOut)
+    }elseif ($linkOut -eq 3){
+        $replacereg = '\..*$'
+        $urlOut = ($urlOut -replace 'https://cdn-1.files.vc/files/')
+        $urlOut = ("https://api.files.vc/files/"+$urlOut)
     }
     Set-Clipboard $urlOut
 }
-        if ($notify){New-BurntToastNotification -Applogo "C:\Program Files\FilesCM\logo.ico" -Text "Files.vc upload","$Messageout","$Urlout"}
-        exit
+        if ($notify){
+            $message = Get-Content response.txt | Select-Object -Index 1
+            $messageOut = ($message -replace '.*    "message": "' -replace '",.*')
+            New-BurntToastNotification -Applogo "C:\Program Files\FilesCM\logo.ico" -Text "Files.vc upload","$Messageout","$Urlout"
+        }
+exit
