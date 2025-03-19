66b2088ca44a5c3180a579538aef6ebed161c718
Andrebeck27 <84735477+Andrebeck27@users.noreply.github.com>
2025-03-19 17:49:03 +0100

    just remove it lol

--- installer/files/settings.ps1
+++ installer/files/settings.ps1
@@ -1,6 +1,3 @@
 $Notify = 1
 $LinkOut = 2
 # ^ (0 = None 1 = CDN; 2 = API)
-# API links lead you directly to the website download page and work well for MP4 streaming
-# CDN links instantly download the file once opened.
-# Changing any value to anything invalid will cause problems.
