c3df81e0144e72192d1dec0c3eba3f307ef6d9ae
Andrebeck27 <84735477+Andrebeck27@users.noreply.github.com>
2025-03-19 17:22:22 +0100

    Add files via upload

---
+++ installer/files/settings.ps1
@@ -0,0 +1,6 @@
+$Notify = 1
+$LinkOut = 2
+# ^ (0 = None 1 = CDN; 2 = API)
+# API links lead you directly to the website download page.
+# CDN links work well for MP4 streaming and instantly download the file once opened.
+# Changing any value to anything invalid will cause problems.
