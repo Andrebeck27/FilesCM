8141ce3d6e91438c3e18bfcc61df3d1edd6ae6f0
Andrebeck27 <84735477+Andrebeck27@users.noreply.github.com>
2025-03-19 17:21:03 +0100

    add settings

--- installer/files/notif.ps1
+++ installer/files/notif.ps1
@@ -1,9 +1,15 @@
+. ./settings.ps1
 $message = Get-Content response.txt | Select-Object -Index 1
 $url = Get-Content response.txt | Select-Object -Index 2
 $messageOut = ($message -replace '.*    "message": "' -replace '",.*')
 $urlOut = ($url -replace '.*    "file_url": "' -replace '",.*')
-New-BurntToastNotification -Applogo "C:\Program Files\FilesCM\logo.ico" -Text "Files.vc upload","$Messageout","$Urlout"
-Set-Clipboard $urlOut
-# hey future me add the exit here
-exit
-# HI
+if ($LinkOut -ne 0){
+    if ($linkOut -eq 2){
+        $replacereg = '\..*$'
+        $urlOut = ($urlOut -replace '.*/' -replace $replacereg)
+        $urlOut = ("https://files.vc/d/dl?hash="+$urlOut)
+    }
+    Set-Clipboard $urlOut
+}
+        if ($notify){New-BurntToastNotification -Applogo "C:\Program Files\FilesCM\logo.ico" -Text "Files.vc upload","$Messageout","$Urlout"}
+        exit
