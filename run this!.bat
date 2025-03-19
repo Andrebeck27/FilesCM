4a8ec9848e717725d178474ee6d78cbc21c3c5f1
Andrebeck27 <84735477+Andrebeck27@users.noreply.github.com>
2025-03-19 17:19:27 +0100

    fix error on path with space

--- run this!.bat
+++ run this!.bat
@@ -1,4 +1,4 @@
 cd installer
 echo %cd% > %temp%\fvcdir.txt
-Powershell Start-Process powershell -Verb runAs -ArgumentList '-ExecutionPolicy Unrestricted -NoProfile -Noexit -File "%cd%\install.ps1"'
+Powershell Start-Process powershell -Verb runAs -ArgumentList '-ExecutionPolicy Unrestricted -File """%cd%\install.ps1"""'
 exit
