da07dc4095d8dffaad826c02faf15c7dad420306
Andrebeck27 <84735477+Andrebeck27@users.noreply.github.com>
2025-03-19 21:53:47 +0100

    removed (?) warning when opening console

--- run this!.bat
+++ run this!.bat
@@ -1,4 +1,5 @@
 cd installer
 echo %cd% > %temp%\fvcdir.txt
+Powershell Unblock-File """%cd%\install.ps1"""
 Powershell Start-Process powershell -Verb runAs -ArgumentList '-ExecutionPolicy Unrestricted -File """%cd%\install.ps1"""'
 exit
