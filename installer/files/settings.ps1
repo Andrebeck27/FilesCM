8b4268ad6438234d8fc97544ffa198137d01ef03
Andrebeck27 <84735477+Andrebeck27@users.noreply.github.com>
2025-03-19 17:43:30 +0100

    whoops

--- installer/files/settings.ps1
+++ installer/files/settings.ps1
@@ -1,6 +1,6 @@
 $Notify = 1
 $LinkOut = 2
 # ^ (0 = None 1 = CDN; 2 = API)
-# API links lead you directly to the website download page.
-# CDN links work well for MP4 streaming and instantly download the file once opened.
+# API links lead you directly to the website download page and work well for MP4 streaming
+# CDN links instantly download the file once opened.
 # Changing any value to anything invalid will cause problems.
