e23bd270490adf9bbd6c140074bc8479ddba8013
Andrebeck27 <84735477+Andrebeck27@users.noreply.github.com>
2025-04-05 17:39:47 +0200

    Add folder support
    Delete temp files, add more context for filesize, remove quotation marks on equal checks for values

--- installer/files/send.bat
+++ installer/files/send.bat
@@ -1,12 +1,16 @@
 @echo off
 if "%~1"=="" goto Noarg
-if "%~z1" GEQ 10000000000 goto toobig
+if %~z1 GEQ 10000000000 goto toobig
 c: & cd "C:\Program Files\FilesCM"
 set /p AccID=<AccountID.txt
 if "%AccID%"=="" goto noacc
 break > "C:\Program Files\FilesCM\response.txt"
 curl -F "file=@%~1" -H "X-Account-ID: %AccID%" https://api.files.vc/upload > "C:\Program Files\FilesCM\response.txt"
 cmd /c start /min "" Powershell -ExecutionPolicy Bypass -File "C:\Program Files\FilesCM\notif.ps1"
+if exist "C:\Program Files\FilesCM\temp.zip" 	(
+												del /f "C:\Program Files\FilesCM\temp.zip"
+												del /f "C:\Program Files\FilesCM\temp.txt"
+												)
 exit
 
 :toobig
@@ -14,9 +18,8 @@ echo Max filesize allowed is 10GB. (10000000000 Bytes)
 echo. 
 set "gbval=%~z1"
 echo The file selected is ^>%gbval:~0,2% Gigabytes. (%~z1 Bytes)
-echo.
+echo Explorer shows sizes in GiB, not GB.
 echo Nothing has been uploaded. Press any key to exit.
-echo.
 pause > nul
 exit
 
@@ -24,7 +27,6 @@ exit
 echo No first argument found.
 echo.
 echo Please run from commandline with a path in quotation marks beside the command, or from context menu, or from dragging a file in.
-echo.
 pause > nul
 exit
 
